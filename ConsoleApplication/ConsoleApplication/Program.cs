7fdf85a2d3e6b03e61c5a40a376553530c50befb
Victor Espinoza <vic.espinozac@gmail.com>
2017-03-13 19:47:15 -0700

    Done

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            long result;
+
+            Console.WriteLine("Type the word");
+
+            Program hash = new Program();
+
+            s = Console.ReadLine();
+
+            result = Hashing(s);
+
+            Console.WriteLine(Hashing(s));
+            Console.Read();
+        }
+
+        public static Int64 Hashing(string s){
+
+            Int64 h;
+            string letters;
+            int i;
+            int index;
+            long inc;
+
+            h = 7;
+            letters = "acdegilmnopratuw";
+
+            for (i=0; i<s.Length; i++){
+                inc = (h * 37);
+                index = letters.IndexOf(s[i]);
+                h = inc + index;
+            }
+            return h;
+        }
+    }
+}
---
+++ ConsoleApplication/Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string word = "leepadg";
+            Int64 result = 680131659347;
+
+            Assert.AreEqual(result,Program.Hashing(word));
+        }
+    }
+}
